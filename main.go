bf75bcf9258a749eae9240fe0eabf74b0c1af883
Knut Ahlers <knut@ahlers.me>
2014-10-12 17:13:06 +0200

    Added container management

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/fsouza/go-dockerclient"
@@ -24,6 +25,12 @@ func orFail(err error) {
 	}
 }
 
+func orLog(err error) {
+	if err != nil {
+		log.Print(err)
+	}
+}
+
 func appendIfMissing(slice []string, s string) []string {
 	for _, e := range slice {
 		if e == s {
@@ -31,6 +38,15 @@ func appendIfMissing(slice []string, s string) []string {
 		}
 	}
 	return append(slice, s)
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 // #### DOCKER ####
@@ -71,7 +87,146 @@ func refreshImages() {
 			Repository: v.Image,
 			Tag:        v.Tag,
 		}, docker.AuthConfiguration{})
-		orFail(err)
+		orLog(err)
+	}
+}
+
+func bootContainer(name string, cfg containerConfig) {
+	newcfg := &docker.Config{
+		AttachStdin:  false,
+		AttachStdout: true,
+		AttachStderr: true,
+		Image:        strings.Join([]string{cfg.Image, cfg.Tag}, ":"),
+		Env:          cfg.Environment,
+	}
+	log.Printf("Creating container %s", name)
+	container, err := dockerClient.CreateContainer(docker.CreateContainerOptions{
+		Name:   name,
+		Config: newcfg,
+	})
+	orLog(err)
+	if err != nil {
+		return
+	}
+
+	hostConfig := docker.HostConfig{
+		Binds:        cfg.Volumes,
+		Privileged:   false,
+		PortBindings: make(map[docker.Port][]docker.PortBinding),
+	}
+
+	for _, v := range cfg.Ports {
+		s := strings.Split(v.Local, ":")
+		hostConfig.PortBindings[docker.Port(v.Container)] = []docker.PortBinding{docker.PortBinding{
+			HostIp:   s[0],
+			HostPort: s[1],
+		}}
+	}
+
+	log.Printf("Starting container %s", container.Name)
+	dockerClient.StartContainer(container.Name, &hostConfig)
+}
+
+func listRunningContainers() ([]docker.APIContainers, error) {
+	containers, err := dockerClient.ListContainers(docker.ListContainersOptions{
+		All: false,
+	})
+	orLog(err)
+	return containers, err
+}
+
+func ensureContainers() {
+	// TODO: Refactor, too long.
+	expectedRunning := []string{}
+	currentRunning, err := listRunningContainers()
+	orLog(err)
+	if err != nil {
+		return
+	}
+	for name, containerCfg := range *cfg {
+		if stringInSlice(serfElector.MyName, containerCfg.Hosts) || stringInSlice("ALL", containerCfg.Hosts) {
+			expectedRunning = append(expectedRunning, name)
+		}
+	}
+
+	// Stop containers not expected to be running
+	for _, v := range currentRunning {
+		allowed := false
+		for _, n := range v.Names {
+			if stringInSlice(strings.Trim(n, "/"), expectedRunning) {
+				allowed = true
+			}
+		}
+		if !allowed {
+			log.Printf("Stopping container %s as it is not expected to be running.", v.ID)
+			err := dockerClient.StopContainer(v.ID, 5)
+			orFail(err)
+			time.Sleep(time.Second * 5)
+		}
+	}
+
+	// Stop containers who are of deprecated images
+	currentRunning, _ = listRunningContainers()
+	images := getImages(false)
+	for _, n := range expectedRunning {
+		repoName := strings.Join([]string{(*cfg)[n].Image, (*cfg)[n].Tag}, ":")
+		currentImageID := "0"
+		for _, i := range images {
+			if stringInSlice(repoName, i.RepoTags) {
+				currentImageID = i.ID
+			}
+		}
+		if currentImageID == "0" {
+			log.Printf("Found no image ID for repo %s", strings.Join([]string{(*cfg)[n].Image, (*cfg)[n].Tag}, ":"))
+			continue
+		}
+		for _, v := range currentRunning {
+			if stringInSlice(fmt.Sprintf("/%s", n), v.Names) && !strings.HasPrefix(currentImageID, v.Image) && v.Image != repoName {
+				log.Printf("Image: %s Current: %s", v.Image, currentImageID)
+				log.Printf("Stopping deprecated container %s", v.ID)
+				err := dockerClient.StopContainer(v.ID, 5)
+				orFail(err)
+				time.Sleep(time.Second * 5)
+				log.Printf("Removing deprecated container %s", v.ID)
+				dockerClient.RemoveContainer(docker.RemoveContainerOptions{
+					ID: v.ID,
+				})
+			}
+		}
+	}
+
+	// Start expected containers
+	currentRunning, _ = listRunningContainers()
+	runningNames := []string{}
+	for _, v := range currentRunning {
+		for _, n := range v.Names {
+			runningNames = append(runningNames, strings.Trim(n, "/"))
+		}
+	}
+	for _, n := range expectedRunning {
+		if !stringInSlice(n, runningNames) {
+			bootContainer(n, (*cfg)[n])
+		}
+	}
+}
+
+func cleanContainers() {
+	runningContainers, err := dockerClient.ListContainers(docker.ListContainersOptions{
+		All: true,
+	})
+	orLog(err)
+	if err != nil {
+		return
+	}
+
+	for _, v := range runningContainers {
+		if strings.HasPrefix(v.Status, "Exited") {
+			log.Printf("Removing stopped container %s", v.ID)
+			err := dockerClient.RemoveContainer(docker.RemoveContainerOptions{
+				ID: v.ID,
+			})
+			orLog(err)
+		}
 	}
 }
 
@@ -113,14 +268,14 @@ func main() {
 		case <-actionTimer.C:
 			log.Print("Action-Tick!")
 
-			//refreshImages()
-			//cleanDangling()
+			refreshImages()
+			ensureContainers()
 
 			actionTimer.Reset(time.Second * 300)
 		case <-cleanupTimer.C:
 			log.Print("Cleanup-Tick!")
 
-			refreshImages()
+			cleanContainers()
 			cleanDangling()
 
 			cleanupTimer.Reset(time.Minute * 30)
