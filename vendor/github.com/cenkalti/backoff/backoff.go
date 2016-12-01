730baf2b7effd7c1cb287b291126cfa234e64196
Knut Ahlers <knut@ahlers.me>
2016-12-01 01:45:51 +0100

    Update godeps

--- vendor/github.com/cenkalti/backoff/backoff.go
+++ vendor/github.com/cenkalti/backoff/backoff.go
@@ -5,18 +5,18 @@ package backoff
 
 import "time"
 
-// BackOff is a backoff policy for retrying an operation.
+// Back-off policy when retrying an operation.
 type BackOff interface {
-	// NextBackOff returns the duration to wait before retrying the operation,
-	// or backoff.Stop to indicate that no more retries should be made.
+	// Gets the duration to wait before retrying the operation or
+	// backoff.Stop to indicate that no retries should be made.
 	//
 	// Example usage:
 	//
 	// 	duration := backoff.NextBackOff();
 	// 	if (duration == backoff.Stop) {
-	// 		// Do not retry operation.
+	// 		// do not retry operation
 	// 	} else {
-	// 		// Sleep for duration and retry operation.
+	// 		// sleep for duration and retry operation
 	// 	}
 	//
 	NextBackOff() time.Duration
@@ -28,25 +28,22 @@ type BackOff interface {
 // Indicates that no more retries should be made for use in NextBackOff().
 const Stop time.Duration = -1
 
-// ZeroBackOff is a fixed backoff policy whose backoff time is always zero,
-// meaning that the operation is retried immediately without waiting, indefinitely.
+// ZeroBackOff is a fixed back-off policy whose back-off time is always zero,
+// meaning that the operation is retried immediately without waiting.
 type ZeroBackOff struct{}
 
 func (b *ZeroBackOff) Reset() {}
 
 func (b *ZeroBackOff) NextBackOff() time.Duration { return 0 }
 
-// StopBackOff is a fixed backoff policy that always returns backoff.Stop for
-// NextBackOff(), meaning that the operation should never be retried.
+// StopBackOff is a fixed back-off policy that always returns backoff.Stop for
+// NextBackOff(), meaning that the operation should not be retried.
 type StopBackOff struct{}
 
 func (b *StopBackOff) Reset() {}
 
 func (b *StopBackOff) NextBackOff() time.Duration { return Stop }
 
-// ConstantBackOff is a backoff policy that always returns the same backoff delay.
-// This is in contrast to an exponential backoff policy,
-// which returns a delay that grows longer as you call NextBackOff() over and over again.
 type ConstantBackOff struct {
 	Interval time.Duration
 }
