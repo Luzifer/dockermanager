d54626d43f7692f9c548db3ebb994ef1d666a34b
Knut Ahlers <knut@ahlers.me>
2017-07-02 17:42:20 +0200

    Change default docker-host to Unix socket
    This is a more sane value now as dockermanager only cares about one host
    instead of being cluster aware. When required TCP connections are still
    usable.
    
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- params.go
+++ params.go
@@ -9,7 +9,7 @@ type dockerManagerParams struct {
 	Config   string `default:"config.yaml" flag:"config,c" description:"Config file or URL to read the config from"`
 	LogLevel string `flag:"log-level" default:"info" description:"Set log level (debug, info, warning, error)"`
 
-	DockerHost    string `default:"tcp://127.0.0.1:2221" flag:"docker-host" env:"DOCKER_HOST" description:"Connection method to the docker server"`
+	DockerHost    string `default:"unix:///var/run/docker.sock" flag:"docker-host" env:"DOCKER_HOST" description:"Connection method to the docker server"`
 	DockerCertDir string `default:"" flag:"docker-certs" description:"Directory containing cert.pem, key.pem, ca.pem for the registry"`
 
 	ConfigLoadInterval   int `default:"10" flag:"configInterval" description:"Sleep time in minutes to wait between config reloads"`
